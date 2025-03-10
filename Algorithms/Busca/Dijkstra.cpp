c1c15c47d3cc1b7d32b53f341a67a9bcd4967da3
ArturRegadas <ahlregadas@gmail.com>
2025-03-09 23:23:47 -0300

    dijkstra.cpp

--- Algorithms/Busca/Dijkstra.cpp
+++ Algorithms/Busca/Dijkstra.cpp
@@ -1,99 +1,68 @@
-#include <iostream>
-#include <unordered_map>
-#include <string>
-#include <vector>
-#include <limits>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+#define INF (int)1e9+10
+#define pb push_back
+#define F first
+#define S second
+#define map unordered_map<int, vector<pair<int , int>>>
 using namespace std;
-int infinity = numeric_limits<int>::max();
 
-int getMin(unordered_map<int, int> prices, vector<int> queue, unordered_map<int, bool> processed){
-    int minus = infinity;
-    int index = -1;
-    for(int i : queue){
-        if(prices[i] < minus && !processed[i]){
-            minus=prices[i];
-            index = i;
-        }
+unordered_map<int, int> parents;
+
+void getPath(int current, vector<int>& ans){
+    if(current != parents[current]){
+        getPath(parents[current], ans);
     }
-    return index;
-
+    ans.pb(current);
 }
 
-vector<int> getPath(unordered_map<int, int> parents, int init, int current){
-    vector<int> path;
-    while(current != init){
-        path.push_back(current);
-        current = parents[current];
-    }
-    path.push_back(init);
-    reverse(path.begin(), path.end());
-    return path;
-
-
-}
-
-vector<int> dijkstra(unordered_map<int, unordered_map<int, int>> graph, int init, int end, int size){
-    
+vector<int> dijkstra(int init, int destiny, map graph){
+    int currPrice, currNode, iPrice, iNode;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
     unordered_map<int, int> prices;
-    unordered_map<int, bool> processed;
-    for(int i = 1; i<=size; i++){
-        prices[i] = infinity;
-        processed[i] = false;
-    }
+    parents[init] = init;
+    for(const auto& [key, value] : graph)prices[key] = INF;
     prices[init] = 0;
+    pair<int , int> current;
+    pq.push({0, init});
+    while(!pq.empty()){
+        current = pq.top();
+        pq.pop();
+        currPrice = current.F;
+        currNode = current.S;
+        if(currPrice > prices[currNode])continue;
+        if(currNode == destiny){
+            vector<int> ans;
+            getPath(destiny, ans);
+            return ans;
 
-    int current;
-    vector<int> queue = {init};
-
-    unordered_map<int, int> parents;
-
-
-    for(int g =0; g<size; g++){
-        current = getMin(prices, queue, processed);
-        if(current == -1){
-            break;
         }
-        if (current == end){
-            return getPath(parents, init, end);
-        }
-        processed[current] = true;
-
-        for(auto i : graph[current]){
-            queue.push_back(i.first);
-            if(prices[current]+i.second< prices[i.first]){
-                prices[i.first]=prices[current]+i.second;
-                parents[i.first] = current;
+        for(pair<int, int> i : graph[currNode]){
+            iPrice = i.S;iNode = i.F;
+            if(prices[currNode]+iPrice < prices[iNode]){
+                parents[iNode] = currNode;
+                prices[iNode] = prices[currNode]+iPrice;
+                pq.push({prices[iNode], iNode});
             }
         }
     }
     return {-1};
-
 }
 
-
 int main(){
+    int vertices,edges, init , destiny;
+    int in, d, p;
+    scanf("%d%d", &vertices, &edges);
 
-    int vertices, edges;
-    unordered_map<int, unordered_map<int, int>> graph;
-
-    cin>>vertices>>edges;
-    
-    int edg1, edg2, wgh;
-    for(int i = 1; i<=edges; i++){
-        cin>>edg1>>edg2>>wgh;
-        graph[edg1][edg2] = wgh;
-        graph[edg2][edg1] = wgh;
+    map graph;
+    for(int i =0;i<edges;++i){
+        scanf("%d%d%d", &in, &d, &p);
+        graph[in].pb({d, p});
+        graph[d].pb({in, p});
     }
-
-    vector<int> path = dijkstra(graph, 1, vertices, vertices);
-
-    for(int i: path){
-        cout<< i<<" ";
-    }
-    cout<<"\n";
-
+    scanf("%d%d", &init, &destiny);
+    vector<int> ans = dijkstra(init, destiny, graph);
+    for(int i =0;i<ans.size()-1;++i)printf("%d ", ans[i]);
+    printf("%d\n", ans.back());
 
 
     return 0;
